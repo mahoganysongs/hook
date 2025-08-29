bef497550443440d05920e7115ee67a281e981c7
Mark Murdoch <mark@anthemsocial.com>
2025-08-29 18:40:49 +0100

    Initial Commit

---
+++ Hook/HookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HookApp.swift
+//  Hook
+//
+//  Created by Mark Murdoch on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct HookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HookTests/HookTests.swift
@@ -0,0 +1,17 @@
+//
+//  HookTests.swift
+//  HookTests
+//
+//  Created by Mark Murdoch on 29/08/2025.
+//
+
+import Testing
+@testable import Hook
+
+struct HookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
